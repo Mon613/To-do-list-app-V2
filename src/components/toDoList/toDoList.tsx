c9617b27da170ea77895cff03146328836146a01
Mon613 <nguyenha61314@gmail.com>
2025-03-03 17:01:58 +0700

    update project

--- src/components/toDoList/toDoList.tsx
+++ src/components/toDoList/toDoList.tsx
@@ -3,55 +3,7 @@ import { Task } from "../../types/Task";
 import userTask from "../../hooks/TaskHook";
 
 const TodoListApp = () => {
-    // const [tasks, setTasks] = useState<Task[]>(
-    //     JSON.parse(localStorage.getItem("tasks") || "[]")
-    // );
-    // const [newTask, setNewTask] = useState("");
-    // const [editingId, setEditingId] = useState<number | null>(null);
-    // const [editingText, setEditingText] = useState("");
 
-    // useEffect(() => {
-    //     localStorage.setItem("tasks", JSON.stringify(tasks))
-    // }, [tasks]);
-    // const addTask = () => {
-    //     if (!newTask.trim()) {
-    //         return (
-    //             alert("Please enter task!!!")
-    //         )
-    //     }
-    //     const task: Task = {
-    //         id: Date.now() + Math.random(),
-    //         text: newTask,
-    //         completed: false
-    //     };
-    //     setTasks([task, ...tasks]);
-    //     setNewTask("");
-    //     // console.log(tasks);
-    // }
-    // const deleteTask = (id: number) => {
-    //     setTasks(tasks.filter((task) => task.id !== id));
-    // }
-    // const editTask = (id: number, text?: string) => {
-    //     if (editingId == id) {
-    //         setTasks(
-    //             tasks.map((task) =>
-    //                 task.id === id ? { ...task, text: editingText } : task
-    //             )
-    //         )
-    //         setEditingId(null);
-    //     } else {
-    //         setEditingId(id);
-    //         setEditingText(text || "");
-    //     }
-
-    // }
-    // const statusTask = (id: number) => {
-    //     setTasks(
-    //         tasks.map((task) =>
-    //             task.id === id ? { ...task, completed: !task.completed } : task
-    //         )
-    //     )
-    // }
     const { tasks, addTask, deleteTask, statusTask, editTask, editingId, editingText, setEditingText } = userTask();
     const [newTask, setNewTask] = useState("");
     const handleAddTask = () => {
